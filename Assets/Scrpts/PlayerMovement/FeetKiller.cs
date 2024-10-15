d109da40e7a5d436ed0f8fcd3363704cbbf13ce8
Vassamo <56084878+Vassamo@users.noreply.github.com>
2024-10-15 14:56:35 +0200

    Enemy/player death with sounds
    so sad :(

---
+++ Assets/Scrpts/PlayerMovement/FeetKiller.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class FeetKiller : MonoBehaviour
+{
+    public Rigidbody2D playerRigBody;
+    const int jumpForce = 20; 
+    private Collider2D feetcol;
+    public AudioSource deathSound;
+
+    private void Start()
+    {
+        //playerRigBody = GetComponent<Rigidbody2D>();
+        feetcol = GetComponent<Collider2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Debug.Log("omg");
+            // Zniknięcie przeciwnika
+
+            Bounce();
+            deathSound.Play();
+            Destroy(other.transform.parent.gameObject);
+        }
+    }
+    private void Bounce()
+    {
+        // Ustaw prędkość gracza w górę
+        playerRigBody.velocity = new Vector2(playerRigBody.velocity.x, jumpForce);
+    }
+}
---
+++ Assets/Scrpts/enemy/EnemyKillPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class EnemyKillPlayer : MonoBehaviour
+{
+    private Collider2D killerRange;
+    public AudioSource playerDeath;
+    public AudioMixerGroup allmusic;
+    float liczbaSekund = 4f;
+    void Start()
+    {
+        killerRange = GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("gracz zostal rozkurwiony na amen");
+            playerDeath.Play();
+            allmusic.audioMixer.SetFloat("BgMusicVolume",-6);
+            StartCoroutine(PrzykładKorutyny());
+        }
+    }
+
+    IEnumerator PrzykładKorutyny()
+    {
+        Time.timeScale = 0f;
+        yield return new WaitForSecondsRealtime(liczbaSekund);
+        // Kod do wykonania po odczekaniu
+        Debug.Log("restart");
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+}
