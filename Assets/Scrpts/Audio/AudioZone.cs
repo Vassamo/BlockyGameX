3a31b6e73eb2b0fc049ac8d9fe73db4b2e07af62
Vassamo <56084878+Vassamo@users.noreply.github.com>
2024-11-22 11:08:03 +0100

    Zone split
    ZoneX/SkyZone/
    
    ZoneY soon

--- Assets/Scrpts/Audio/AudioZone.cs
+++ Assets/Scrpts/Audio/AudioZone.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.Audio;
+﻿using UnityEngine;
 
 public class AudioZone : MonoBehaviour
 {
@@ -13,40 +11,14 @@ public class AudioZone : MonoBehaviour
         public AudioSource source2;
     }
 
-    [System.Serializable]
-    public class AudioPointY
-    {
-        public float y1;
-        public float y2;
-        public AudioSource[] audioSources;
-        public float EndingVolumeDB;
-        public float EndingFilter;
-     
-    }
-
-    public AudioPoint[] pointsX;
-    public AudioPointY[] pointsY;
+    public AudioPoint[] points;
     public Transform playerTransform;
-    private float initVol;
-    private float initHighFiler;
-    public AudioMixerGroup MusicBgMixer;
-
-    private float currentT = 0f; // Aktualna wartość t
-    private float targetT = 0f; // Docelowa wartość t
-    private float speedMultiplier = 1.5f;
-
-    private void Start()
-    {
-        MusicBgMixer.audioMixer.GetFloat("BgMusicVolume", out initVol);
-        MusicBgMixer.audioMixer.GetFloat("BGHighPass", out initHighFiler);
-    }
 
     private void Update()
     {
         float playerX = playerTransform.position.x;
-        float playerY = playerTransform.position.y;
 
-        foreach (var point in pointsX)
+        foreach (var point in points)
         {
             if (playerX < point.x1)
             {
@@ -65,39 +37,17 @@ public class AudioZone : MonoBehaviour
                 point.source2.volume = t;
             }
         }
-
-        foreach (var point in pointsY)
-        {
-                targetT = (playerY - point.y1) / (point.y2 - point.y1); //old t
-                targetT = Mathf.Clamp01(targetT);
-
-                currentT = Mathf.Lerp(currentT, targetT, Time.deltaTime * speedMultiplier);
-
-                float currentVolume = Mathf.Lerp(initVol, -point.EndingVolumeDB, currentT);
-                float currentFiler = Mathf.Lerp(initHighFiler, point.EndingFilter, currentT);
-
-                MusicBgMixer.audioMixer.SetFloat("BgMusicVolume", currentVolume);
-                MusicBgMixer.audioMixer.SetFloat("BGHighPass", currentFiler);
-        }
     }
 
     private void OnDrawGizmos()
     {
-        if (pointsX == null) return;
-        if (pointsY == null) return;
+        if (points == null) return;
 
         Gizmos.color = Color.red;
-        foreach (var point in pointsX)
+        foreach (var point in points)
         {
             Gizmos.DrawLine(new Vector3(point.x1, -1000, 0), new Vector3(point.x1, 1000, 0));
             Gizmos.DrawLine(new Vector3(point.x2, -1000, 0), new Vector3(point.x2, 1000, 0));
         }
-
-        Gizmos.color = Color.blue;
-        foreach (var point in pointsY)
-        {
-            Gizmos.DrawLine(new Vector3(-1000, point.y1, 0), new Vector3(1000, point.y1, 0)); // Pozioma linia na y1
-            Gizmos.DrawLine(new Vector3(-1000, point.y2, 0), new Vector3(1000, point.y2, 0)); // Pozioma linia na y2
-        }
-}
+    }
 }
---
+++ Assets/SkyAudioZone.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class SkyAudioZone : MonoBehaviour
+{
+    [System.Serializable]
+    public class AudioPointY
+    {
+        public float y1;
+        public float y2;
+        public AudioSource[] audioSources;
+        public float EndingVolumeDB;
+        public float EndingFilter;
+
+    }
+
+    public AudioPointY[] pointsY;
+    public Transform playerTransform;
+    public AudioMixerGroup MusicBgMixer;
+    private float initVol;
+    private float initHighFiler;
+
+    private float currentT = 0f; // Aktualna wartość t
+    private float targetT = 0f; // Docelowa wartość t
+    private float speedMultiplier = 1.5f;
+
+    private void Start()
+    {
+        MusicBgMixer.audioMixer.GetFloat("BgMusicVolume", out initVol);
+        MusicBgMixer.audioMixer.GetFloat("BGHighPass", out initHighFiler);
+    }
+
+    private void Update()
+    {
+        float playerY = playerTransform.position.y;
+
+        foreach (var point in pointsY)
+        {
+            targetT = (playerY - point.y1) / (point.y2 - point.y1); //old t
+            targetT = Mathf.Clamp01(targetT);
+
+            currentT = Mathf.Lerp(currentT, targetT, Time.deltaTime * speedMultiplier);
+
+            float currentVolume = Mathf.Lerp(initVol, -point.EndingVolumeDB, currentT);
+            float currentFiler = Mathf.Lerp(initHighFiler, point.EndingFilter, currentT);
+
+            MusicBgMixer.audioMixer.SetFloat("BgMusicVolume", currentVolume);
+            MusicBgMixer.audioMixer.SetFloat("BGHighPass", currentFiler);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (pointsY == null) return;
+
+        Gizmos.color = Color.blue;
+        
+        foreach (var point in pointsY)
+        {
+            Gizmos.DrawLine(new Vector3(-1000, point.y1, 0), new Vector3(1000, point.y1, 0)); // Pozioma linia na y1
+            Gizmos.DrawLine(new Vector3(-1000, point.y2, 0), new Vector3(1000, point.y2, 0)); // Pozioma linia na y2
+        }
+    }
+
+}
